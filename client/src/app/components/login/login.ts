0a32c0ef117e4ff4e26e9c6b5582b9f3493d644d
Ridam3010 <ridambaranwal30@gmail.com>
2026-08-22 00:33:19 +0530

    login

--- client/src/app/components/login/login.ts
+++ client/src/app/components/login/login.ts
@@ -1,9 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Auth } from '../../services/auth';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+  username = '';
+  password = '';
+
+  private authService = inject(Auth);
+  private router = inject(Router);
+
+  onSubmit() {
+    const credentials = { username: this.username, password: this.password };
+
+    this.authService.login(credentials).subscribe({
+      next: (token) => {
+        console.log("Login Success, Token saved");
+
+        this.router.navigate(['/dashboard']);
+      },
+      error: (err) => {
+        console.log("Login failed", err);
+        alert("Invalid username or password");
+      }
+    });
+  }
+}
--- client/src/app/services/auth.ts
+++ client/src/app/services/auth.ts
@@ -1,6 +1,22 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
-export class Auth {}
+export class Auth {
+  private http = inject(HttpClient);
+  login(credentials: any) {
+    return this.http.post('http://localhost:8080/api/v1/auth/login', credentials, { responseType: 'text' }).pipe(tap(token => {
+      localStorage.setItem('jwt_token', token);
+    })
+    );
+  }
+  getToken(){
+    return localStorage.getItem('jwt_token');
+  }
+  isLoggedIn(){
+    return !!this.getToken();
+  }
+}
