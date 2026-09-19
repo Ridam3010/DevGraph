11d4a7c827b3a76da3c592ee6ffb9104dc775546
Ridam3010 <ridambaranwal30@gmail.com>
2026-09-19 23:52:51 +0530

    interceptor

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 import { Login } from './components/login/login';
+import { Dashboard } from './components/dashboard/dashboard'; 
+import { authGuard } from './guards/auth.guard'; // 1. Import the Bouncer
 
 export const routes: Routes = [
   { path: 'login', component: Login },
-  { path: '', redirectTo: '/login', pathMatch: 'full' } // If they visit the root URL, send them to login
+  // 2. Put the Bouncer in front of the dashboard!
+  { path: 'dashboard', component: Dashboard, canActivate: [authGuard] }, 
+  { path: '', redirectTo: '/login', pathMatch: 'full' }
 ];
---
+++ client/src/app/interceptors/auth-interceptor.ts
@@ -0,0 +1,21 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { Auth } from '../services/auth';
+
+export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  const authService = inject(Auth);
+  const token = authService.getToken();
+
+  // If we have a token, clone the request and add the Authorization header
+  if (token) {
+    const clonedRequest = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+    return next(clonedRequest); // Send the modified request
+  }
+
+  // If we don't have a token, just send the original request as-is
+  return next(req); 
+};
